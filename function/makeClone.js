74058fd2eadaca66e38c2989f4f1266bd659efff
minhquang3225 <minhquang3225@outlook.com>
2021-01-28 00:55:55 +0700

    update makeCloneJS lên v2.0

--- function/makeClone.js
+++ function/makeClone.js
@@ -1,87 +1,112 @@
-import { getEle, getEles } from "../function/global.js";
+//makeClone v2.0
 
-export default function makeClone() {
+import { getEle, getEles } from "./global.js";
 
-    const makeClone = (DataObjArr) => {
-        DataObjArr.forEach((obj) => {
-            let origins = document.querySelectorAll(obj.className);
-            origins.forEach(origin => {
-                let parentOrigin = origin.parentElement;            //Nơi chứa cụm bản sao
-                let levelData = obj.levelData ? obj.levelData : 1;
-                let content = ''                               //Cụm bản sao sau khi copy ra
-                let finalContent = '';
+export const makeClone = (DataObjArr, data) => {
 
-                for (let numClone = 0; numClone < obj.numClone; numClone++) {
-                    content += changeOrigin(origin, parentOrigin, numClone, levelData).outerHTML;
-                }
-                for (let i = 0; i < obj.loopTimes; i++) {
-                    finalContent += content;
-                }
-                parentOrigin.innerHTML = finalContent;
+    DataObjArr.forEach((obj) => {
+        let originNodes = getEles(obj.className);
+        originNodes.forEach(originNode => {
+            let parentNode = originNode.parentElement;            //Nơi chứa cụm bản sao
+            let levelData = obj.levelData ? obj.levelData : 1;
+            let finalHTML = '';
 
-            })
-        })
-    };
-    const changeOrigin = (origin, parentOrigin, numClone, levelData) => {
-        switch (levelData) {
-            case 1:
-                dataElements = parentOrigin.querySelectorAll("[data-sets^=data]");
-                dataBindings = parentOrigin.querySelectorAll("[data-binding]");
-                break;
-            case 2:
-                dataElements = parentOrigin.querySelectorAll("[data-sets^=lv2-data]");
-                break;
-            case 3:
-                dataElements = parentOrigin.querySelectorAll("[data-sets^=lv3-data]");
-                break;
-        }
-        dataElements.forEach(dataElement => {
-            let E_data_sets = dataElement.getAttribute('data-sets') ? dataElement.getAttribute('data-sets').split(' ') : [];
-            let E_data_props = dataElement.getAttribute('data-props') ? dataElement.getAttribute('data-props').split(' ') : [];
-
-            for (const i in E_data_sets) {
-                let E_data_set = E_data_sets[i];
-                let E_data_prop = E_data_props[i];
-
-                switch (E_data_prop) {
-                    case 'text':
-                        let textNode = dataElement.childNodes[0];
-
-                        textNode.nodeValue = data[E_data_set][numClone];
-                        break;
-
-                    case 'src':
-                        dataElement.src = data[E_data_set][numClone]
-                        break;
-                    case 'link':
-                        dataElement.href = data[E_data_set][numClone];
-                        break;
-                    case 'linkText':
-                        dataElement.href = data[E_data_set][numClone];
-                        let linkTextNode = dataElement.childNodes[0];
-                        linkTextNode.nodeValue = data[E_data_set][numClone];
-                        break;
-                    case 'data-src':
-                        dataElement.setAttribute("data-src", data[E_data_set][numClone]);
-                        let srcNode = dataElement.childNodes[0];
-                        srcNode.nodeValue = data[E_data_set][numClone];
-                        break;
-                    default:
-                        dataElement.setAttribute(E_data_prop, data[E_data_set][numClone])
-                        break;
-                }
+            // cloneHTML = cloneOrigin(originNode, parentNode, obj.numClone)
+            for (let cloneInx = 0; cloneInx < obj.numClone; cloneInx++) {
+                let cloneNode = originNode.cloneNode(true)
+                let filterElements = getEles("[data-sets]", cloneNode);
+                let bindingElements = getEles("[data-binding]", cloneNode);
+                // console.log(bindingElements);
+    
+    
+                filterElements.forEach(filterElement => {
+                    let data_set_arr = filterElement.getAttribute('data-sets') ? filterElement.getAttribute('data-sets').split(' ') : [];
+                    let data_prop_arr = filterElement.getAttribute('data-props') ? filterElement.getAttribute('data-props').split(' ') : [];
+                    let isRemoveData = true;
+                   
+                    for (const i in data_set_arr) {
+                        let data_set = data_set_arr[i];
+                        let data_prop = data_prop_arr[i]
+                        
+                        switch (data_prop) {
+                            case 'text':
+                                let textNode = filterElement.childNodes[0];
+    
+                                textNode.nodeValue = data[data_set][cloneInx];
+                                break;
+    
+                            case 'src':
+                                filterElement.src = data[data_set][cloneInx]
+                                break;
+                            case 'link':
+                                filterElement.href = data[data_set][cloneInx];
+                                break;
+                            case 'linkText':
+                                filterElement.href = data[data_set][cloneInx];
+                                let linkTextNode = filterElement.childNodes[0];
+                                linkTextNode.nodeValue = data[data_set][cloneInx];
+                                break;
+                            case 'data-src':
+                                filterElement.setAttribute("data-src", data[data_set][cloneInx]);
+                                let srcNode = filterElement.childNodes[0];
+                                srcNode.nodeValue = data[data_set][cloneInx];
+                                break;
+                            case 'class':
+                                for (let i in data[data_set].length) {
+                                    data[data_set][i] && filterElement.classList.remove(data[data_set][i]);
+                                }
+                                data[data_set][cloneInx] && filterElement.classList.add(data[data_set][cloneInx])
+                                break;
+                            case 'binding':
+                                let bindingArr = data[data_set][cloneInx].split(', ');
+                                filterElement.setAttribute('data-sets', bindingArr[0]);
+                                filterElement.setAttribute('data-props', bindingArr[1]);
+                                isRemoveData = false;
+                                break;
+                            default:
+                                filterElement.setAttribute(data_prop, data[data_set][cloneInx])
+                                break;
+                        }
+                    }
+    
+                    isRemoveData && (()=>{
+                        filterElement.removeAttribute('data-props')
+                        filterElement.removeAttribute('data-sets')
+                    }).call(this)
+                    
+    
+                })
+                // console.log(bindingElements);
+                bindingElements &&
+                    bindingElements.forEach(bindingElement => {
+    
+                        console.log(bindingElement.getAttribute('data-binding'));
+    
+                        let dataValue = data[bindingElement.getAttribute('data-binding')][cloneInx] ? data[bindingElement.getAttribute('data-binding')][cloneInx].split(', ') : [];
+                        let dataNames = dataValue[0];
+                        let dataProps = dataValue[1];
+    
+                        bindingElement.removeAttribute('data-binding');
+                        bindingElement.setAttribute('data-props', dataProps);
+                        bindingElement.setAttribute('data-sets', dataNames);
+    
+                    })
+    
+                parentNode.appendChild(cloneNode)
             }
-
+    
+            parentNode.removeChild(originNode)
+            for (let i = 0; i < obj.loopTimes; i++) {
+                finalHTML += parentNode.innerHTML;
+            }
+            parentNode.innerHTML = finalHTML;
 
         })
-        dataBindings.forEach(dataBinding => {
-            dataValue = data[dataBinding.getAttribute('data-binding')][numClone] ? data[dataBinding.getAttribute('data-binding')][numClone].split(', ') : [];
-            dataNames = dataValue[0];
-            dataProps = dataValue[1];
+    })
+    // function cloneOrigin(originNode, parentNode, numClone) {
+        
 
-            dataBinding.setAttribute('data-props', dataProps);
-            dataBinding.setAttribute('data-sets', dataNames);
-        })
-        return origin;
-    }
+    //     return parentNode;
+    // }
+
 }
--- scripts/makeClone.js
+++ scripts/makeClone.js
@@ -1,29 +1,32 @@
-// import { makeClone } from "../function/makeClone.js"
-import {getData} from "../function/getData.js"
+import { makeClone } from "../function/makeClone.js"
+import { getData } from "../function/getData.js"
 
 
-let getDataValue= async ()=>{
-    let data = await getData('../data/data.json');
-    console.log(data);
-}
+(async () => {
+    let data = await getData("../data/data.json");
+    await makeClone([
+        {
+            className: '.anime-list__item',
+            numClone: 3,
+            loopTimes: 1,
+            // levelData: 1,
+        },
+        {
+            className: '.detail-list__item',
+            numClone: 3,
+            loopTimes: 1,
+            // levelData: 1,
+        },
+        {
+            className: '.avatar-list__item',
+            numClone: 3,
+            loopTimes: 1,
+            // levelData: 2,
+        },
+    ], data)
+}).call(this);
 
-getDataValue();
 
 
 
 
-
-// makeClone([
-//     {
-//         className: '.anime-list__item',
-//         numClone: 6,
-//         loopTimes: 1,
-//         // levelData: 1,
-//     },
-//     {
-//         className: '.detail-list__item',
-//         numClone: 3,
-//         loopTimes: 1,
-//         levelData: 2,
-//     },
-// ])
